1d7431b494945f22c3abafdbf6ccbc8e483c0151
shashank <shashankchaudhary250341@gmail.com>
2022-12-13 15:03:21 +0530

    some modifications

--- clock.js
+++ clock.js
@@ -1,107 +1,93 @@
-function Time(){
-    
-    let dt = new Date();
-    let hour = dt.getHours()
-    let min = dt.getMinutes()
-    let sec = dt.getSeconds()
-    if(hour>=12){
-        document.querySelector("#session").textContent = "PM"
+let liveTime = () => {
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let zone = document.getElementById("zone");
+  
+    //   AM PM
+    hrs >= 12 ? (zone.innerHTML = "PM") : (zone.innerHTML = "AM");
+  
+    //   Greet message according to live clock
+    if (hrs < 12) {
+      var greeting = "Good Morning, Adarsh!";
     }
-    else{
-        document.querySelector("#session").textContent = "AM"
-
+    if (hrs >= 12 && hrs <= 18) {
+      var greeting = "Good Afternoon, Adarsh!";
     }
-
-
-
-    if(hour>12){
-        hour = hour-12
+    if (hrs >= 18 && hrs <= 24) {
+      var greeting = "Good Evening, Adarsh!";
     }
-    
-    document.querySelector("#hour").textContent = `${hour}`;
-    document.querySelector("#min").textContent= `${min}`;
-    document.querySelector("#sec").textContent=`${sec}`;
-
-}
-
-
-setInterval(Time,1000)  // To make clock dynamic , caling the function at each interval of 1 second.
-
-
-
-function fun(){
-    document.querySelector("#alarm-btn").style.background="linear-gradient(to left,#6E54EC 25%,#CB52F8,#FC5EFF,#F1A7C5)"
-
-
-
-    let wakeTime = document.querySelector("#drop-menu1").value;
-    let lunchTime = document.querySelector("#drop-menu2").value;
-    let napTime = document.querySelector("#drop-menu3").value;
-    let nightTime = document.querySelector("#drop-menu4").value;
-
-    let wakeArr = wakeTime.split(" ")
-    let lunchArr = lunchTime.split(" ")
-    let napArr = napTime.split(" ")
-    let sleepArr = nightTime.split(" ")
-
-
-    
-
-    // time message box
-    document.querySelector(".data").style.display="block"
-    
-    document.querySelector(".wakeData").textContent=`Wake Up Time : ${wakeTime}`;
-    document.querySelector(".lunchData").textContent=`Lunch Time : ${lunchTime}`;
-    document.querySelector(".napData").textContent=`Nap Time : ${napTime}`;
-    document.querySelector(".nightData").textContent=`Night Time : ${nightTime}`;
-
-
-
-    // image changing according to current time
-
-    let currentTime = document.querySelector("#hour").textContent;
-    let currentSession = document.querySelector("#session").textContent
-
-    if(Number(currentTime)>=Number(wakeArr[0]) && Number(currentTime)<Number(wakeArr[3]) && currentSession===wakeArr[4]) {
-
-        document.querySelector(".img").src="./assets/morning.jpg"
-        document.querySelector(".msg1").textContent = "GOOD MORNING!! WAKE UP!!"
-        document.querySelector(".msg2").innerHTML = "GRAB SOME HEALTHY BREAKFAST!!"
+    document.getElementById("greet-text").innerHTML = greeting;
+  
+    //   12 hour format
+    if (hrs > 12) {
+      hrs -= 12;
     }
-
-    else if(Number(currentTime)>=Number(lunchArr[0]) && Number(currentTime)<Number(lunchArr[3]) && currentSession===lunchArr[4]){
-        document.querySelector(".img").src="./assets/lunch_image@2x.jpg"
-        document.querySelector(".msg1").textContent = "GOOD AFTERNOON!! TAKE SOME SLEEP!!"
-        document.querySelector(".msg2").textContent = "LET'S HAVE SOME LUNCH!!"
-
-
+  
+    //   live clock time render
+    document.getElementById("hours").innerHTML = hrs;
+    document.getElementById("minutes").innerHTML = min;
+    document.getElementById("seconds").innerHTML = sec;
+  };
+  
+  setInterval(liveTime);
+  
+  const button = document.getElementById("btn");
+  button.addEventListener("click", () => {
+      buttonFunctionality();
+  });
+  
+  let buttonFunctionality = () => {
+      textDisplay();
+      changeDisplay();
+  }
+  
+  // to display text from select option
+  function textDisplay() {
+    let wakeValue = document.getElementById("wakeValue").value;
+    document.getElementById("displayWake").innerHTML = wakeValue;
+    let lunchValue = document.getElementById("lunchValue").value;
+    document.getElementById("displayLunch").innerHTML = lunchValue;
+    let napValue = document.getElementById("napValue").value;
+    document.getElementById("displayNap").innerHTML = napValue;
+  //   changeDisplay();
+  }
+  
+  // text and image change
+  
+  let changeDisplay = () => {
+      // console.log("changedisplaywork")
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+  
+    hrs >= 12 ? (zone.innerHTML = "PM") : (zone.innerHTML = "AM");
+  
+    if (hrs > 12) {
+      hrs -= 12;
+      hr = hrs;
+    } else {
+      hr = hrs;
     }
-    else if(Number(currentTime)>=Number(napArr[0]) && Number(currentTime)<Number(napArr[3]) && currentSession===napArr[4]){
-        document.querySelector(".img").src="./assets/Group 5183@2x.jpg"
-        document.querySelector(".msg1").textContent = "GODD EVENING!!!!"
-        document.querySelector(".msg2").textContent = "STOP! YAWNING,GET SOME TEA..!IT'S JUST EVENING."
-        
+  
+    let arr1 = wakeValue.value.split(" ");
+    let arr2 = lunchValue.value.split(" ");
+    let arr3 = napValue.value.split(" ");
+  
+    if (arr1[0] == hr && arr1[1] == zone.innerHTML) {
+      console.log("if Morning");
+      document.getElementById('changeMessage').innerText="Grab some Healthy BreakFast!!!";
+      document.getElementById('images').src = "assets/img_1.svg"
     }
-    else if(Number(currentTime)>=Number(sleepArr[0]) && Number(currentTime)<Number(sleepArr[3]) && currentSession===sleepArr[4]){
-        document.querySelector(".img").src="./assets/Group 5194@2x.jpg";
-        document.querySelector(".img").style.height= "250px"
-        document.querySelector(".msg1").textContent = "GOOD NIGHT!!"
-        document.querySelector(".msg2").textContent = "CLOSE YOUR EYES AND GO TO SLEEP!!"
-        
+    else if(arr2[0] == hr && arr2[1] == zone.innerHTML) {
+      console.log("if Lunch");
+      document.getElementById('changeMessage').innerText="Let's have some lunch";
+      document.getElementById('images').src = "assets/lunch-img.svg"
+    } else if(arr3[0] == hr && arr3[1] == zone.innerHTML){
+      console.log("if Nap");
+      document.getElementById('changeMessage').innerText="Sleep is the best meditation!";
+      document.getElementById('images').src = "assets/dinner-img.svg";
     }
-    
-    
-
-
-    
-
-
-
-setInterval(fun,1000)  // To make the page dynamic according to time set, calling the function at each interval of 1 second.
-
-
-
-}
-
-
-document.querySelector("#alarm-btn").addEventListener("click",fun);
+  };
